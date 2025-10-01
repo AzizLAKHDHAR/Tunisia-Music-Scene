ef4fa35b4fc90fdbb4e8997e90e5f3e9460fcad7
AzizLAKHDHAR <mohamedaziz.lakhdhar@esprit.tn>
2025-10-01 12:03:58 +0100

    vite config fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // Set base path for GitHub Pages deployment
+  base: process.env.NODE_ENV === 'production' ? '/Tunisia-Music-Scene/' : '/',
   server: {
     host: "::",
     port: 8080,
